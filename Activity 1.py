8658f4482fd1409edc43c07f2b38a8208a284b08
Goopbloop <ami5455.idk@gmail.com>
2025-04-29 20:12:05 +0600

    Lesson 3 Activity 1

---
+++ Activity 1.py
@@ -0,0 +1,7 @@
+print("Welcome to the world of programming")
+
+print(3)
+
+print("Hi!", 3)
+
+print("Wow", end= "!")
---
+++ Lesson 3/Activity 1.py
@@ -0,0 +1,11 @@
+a = 14
+print("type of the variable a", type(a))
+
+b = 7.9
+print("type of the variable b", type(b))
+      
+c = "Gloop"
+print("type of the variable c", type (c))
+
+d = False
+print("type of the variable d", type (d))
