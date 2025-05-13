28ae6cb3a804946297c42767197323ab1326eb30
Goopbloop <ami5455.idk@gmail.com>
2025-05-13 20:32:51 +0600

    Lesson 6 Activity 2

---
+++ Lesson 6/Activity 2.py
@@ -0,0 +1,23 @@
+a = 16 
+b = 18 
+c = 18
+
+print(a != b)
+print(c != b)
+
+a = "PYTHON"
+b = "CODING"
+
+if a != b:
+    print(a, "and", b, "are different.")
+
+a = 5
+b = 6
+
+if (a == 1) != (b == 6):
+    print("BoopGloop")
+
+a = int(input("Enter a Number: "))
+
+if a%2 != 0 :
+    print(a, "Is not an even number.")
