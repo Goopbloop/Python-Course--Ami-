210c2d9693f723b31fc3016f10ab842b03203d6c
Goopbloop <ami5455.idk@gmail.com>
2025-05-20 20:02:51 +0600

    Lesson 8 Activity 1

---
+++ Lesson 8/Activity 1.py
@@ -0,0 +1,19 @@
+a = 5
+b = 4 
+c = 20
+d = 3
+f = 0
+
+f = (b + d) * a / c 
+print("Value of B + D x A / C =", f)
+
+name = "Harry"
+age = 4
+
+if (name == "Harry" or name == "Ron") and age > 4:
+    print("wowzers!")
+
+else:
+    print("YOu FaIleD!")
+
+
