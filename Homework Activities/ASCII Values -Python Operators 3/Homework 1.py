a3075d1c1d521fe104612d41bc5c0f7b161f85f1
Goopbloop <ami5455.idk@gmail.com>
2025-05-20 19:35:07 +0600

    Homework-Python Operators III

---
+++ Homework Activities/ASCII Values -Python Operators 3/Homework 1.py
@@ -0,0 +1,9 @@
+
+
+print("Uppercase letters:")
+for letter in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
+    print(letter, ord(letter))
+
+print("\nLowercase letters:")
+for letter in "abcdefghijklmnopqrstuvwxyz":
+    print(letter, ord(letter))
